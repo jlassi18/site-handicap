05ffa2575ba64951b19f60e33dcf2f78eb8bff40
jlassi18 <93451933+jlassi18@users.noreply.github.com>
2024-11-19 20:36:40 +0100

    Add files via upload

---
+++ site handicap/types.js
@@ -0,0 +1,5 @@
+function toggleDetails(detailsId) {
+    const details = document.getElementById(detailsId);
+    details.style.display = (details.style.display === "block") ? "none" : "block";
+}
+
